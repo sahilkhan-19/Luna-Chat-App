f68992696464ee0f57fce7ab2834da6924520a00
sahil <sahilkhan19info@gmail.com>
2026-04-14 11:22:16 +0530

    fix background image

--- server/server.js
+++ server/server.js
@@ -59,7 +59,12 @@ app.use("/api/auth", userRouter);
 await connectDB();
 
 //start the server
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+}
+
+//Export server for vercel
+export default server;
